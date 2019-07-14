54ff331c144a1234cc4066243520f4ec7181e846
pratik <pratik.ranjan@testbook.com>
2019-07-14 19:37:58 +0530

    navigation

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -21,6 +22,10 @@ class TitleFragment : Fragment() {
            val binding: FragmentTitleBinding = DataBindingUtil.inflate(
                    inflater,R.layout.fragment_title,container,false
            )
+        binding.playButton.setOnClickListener { view: View->
+            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+
+        }
         return binding.root
 
     }
